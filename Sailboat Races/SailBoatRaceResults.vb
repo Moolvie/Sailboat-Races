4a2eca3ea444aa143f81a81318bf0fe20cb0d2a0
dougenos <dougenos@anscomachine.com>
2017-02-10 15:51:45 -0500

    Added code to set focus to the textbox with the error and select the text so the user doesn't have to do anything but retype the number

--- Sailboat Races/SailBoatRaceResults.vb
+++ Sailboat Races/SailBoatRaceResults.vb
@@ -115,54 +115,91 @@
                                                     R4B2Finish = Convert.ToInt16(R4B2.Text)
                                                     Try
                                                         R4B3Finish = Convert.ToInt16(R4B3.Text)
+
                                                         ' calculate the totals for each boat
                                                         Results1.Text = Convert.ToString(R1B1Finish + R2B1Finish + R3B1Finish + R4B1Finish)
                                                         Results2.Text = Convert.ToString(R1B2Finish + R2B2Finish + R3B2Finish + R4B2Finish)
                                                         Results3.Text = Convert.ToString(R1B3Finish + R2B3Finish + R3B3Finish + R4B3Finish)
                                                         ' race four error trapping
                                                     Catch ex As Exception
+                                                        R4B3.Focus()
+                                                        R4B3.SelectionStart = 0
+                                                        R4B3.SelectionLength = R4B3.Text.Length
                                                         ErrorProvider1.SetError(R4B3, "Please try again, and enter an integer.")
                                                     End Try
                                                 Catch ex As Exception
+                                                    R4B2.Focus()
+                                                    R4B2.SelectionStart = 0
+                                                    R4B2.SelectionLength = R4B2.Text.Length
                                                     ErrorProvider1.SetError(R4B2, "Please try again, and enter an integer.")
                                                 End Try
                                             Catch ex As Exception
+                                                R4B1.Focus()
+                                                R4B1.SelectionStart = 0
+                                                R4B1.SelectionLength = R4B1.Text.Length
                                                 ErrorProvider1.SetError(R4B1, "Please try again, and enter an integer.")
                                             End Try
                                             ' end of race four error trapping
 
                                             ' beginning of race three error trapping
                                         Catch ex As Exception
+                                            R3B3.Focus()
+                                            R3B3.SelectionStart = 0
+                                            R3B3.SelectionLength = R3B3.Text.Length
                                             ErrorProvider1.SetError(R3B3, "Please try again, and enter an integer.")
                                         End Try
                                     Catch ex As Exception
+                                        R3B2.Focus()
+                                        R3B2.SelectionStart = 0
+                                        R3B2.SelectionLength = R3B2.Text.Length
                                         ErrorProvider1.SetError(R3B2, "Please try again, and enter an integer.")
                                     End Try
                                 Catch ex As Exception
+                                    R3B1.Focus()
+                                    R3B1.SelectionStart = 0
+                                    R3B1.SelectionLength = R3B1.Text.Length
                                     ErrorProvider1.SetError(R3B1, "Please try again, and enter an integer.")
                                 End Try
                                 ' end of race three error trapping
 
                                 ' Begin Race two error trapping
                             Catch ex As Exception
+                                R2B3.Focus()
+                                R2B3.SelectionStart = 0
+                                R2B3.SelectionLength = R2B3.Text.Length
                                 ErrorProvider1.SetError(R2B3, "Please try again, and enter an integer.")
                             End Try
                         Catch ex As Exception
+                            R2B2.Focus()
+                            R2B2.SelectionStart = 0
+                            R2B2.SelectionLength = R2B2.Text.Length
                             ErrorProvider1.SetError(R2B2, "Please try again, and enter an integer.")
                         End Try
                     Catch ex As Exception
+                        R2B1.Focus()
+                        R2B1.SelectionStart = 0
+                        R2B1.SelectionLength = R2B1.Text.Length
                         ErrorProvider1.SetError(R2B1, "Please try again, and enter an integer.")
                     End Try
                     ' End race two error trapping
 
                     ' Begin race one error trapping
                 Catch ex As Exception
+                    R1B3.Focus()
+                    R1B3.SelectionStart = 0
+                    R1B3.SelectionLength = R1B3.Text.Length
                     ErrorProvider1.SetError(R1B3, "Please try again, and enter an integer.")
                 End Try
             Catch ex As Exception
+                R1B2.Focus()
+                R1B2.SelectionStart = 0
+                R1B2.SelectionLength = R1B2.Text.Length
                 ErrorProvider1.SetError(R1B2, "Please try again, and enter an integer.")
             End Try
         Catch ex As Exception
+            R1B1.Focus()
+            R1B1.SelectionStart = 0
+            R1B1.SelectionLength = R1B1.Text.Length
             ErrorProvider1.SetError(R1B1, "Please try again, and enter an integer.")
         End Try
     End Sub
